0fc8af290bbdd6cd5f261d55e9dd98303dda7faa
galhajaj <galhajaj@gmail.com>
2016-03-10 00:02:12 +0200

    Initial commit
    assets

---
+++ Assets/Scripts/Shell/ShellCollision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShellCollision : MonoBehaviour 
+{
+	private bool _isCollided = false;
+	private int _framesCounterSinceCollision = 0;
+	// ================================================================================================ //
+	void Start () 
+	{
+	
+	}
+	// ================================================================================================ //
+	void Update () 
+	{
+		if (_isCollided)
+			_framesCounterSinceCollision++;
+		if (_framesCounterSinceCollision == 3)
+			Destroy(gameObject); // try
+	}
+	// ================================================================================================ //
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log ("Collision...");
+		_isCollided = true;
+		//Destroy(gameObject);
+	}
+	// ================================================================================================ //
+	/*void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log ("Trigger...");
+		//if(other.gameObject.tag=="bullet")
+		Destroy(gameObject);    
+	}*/
+	// ================================================================================================ //
+}
---
+++ Assets/Scripts/Tank/TurretMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretMovement : MonoBehaviour 
+{
+	public Transform Turret;
+	public float RotateSpeed;
+	// ================================================================================================ //
+	void Start () 
+	{
+	
+	}
+	// ================================================================================================ //
+	void Update () 
+	{
+		// turn right & left
+		if (Input.GetKey (KeyCode.D)) 
+		{
+			Turret.gameObject.transform.Rotate( new Vector3(0, 0, -RotateSpeed * Time.deltaTime), Space.Self );
+		}
+		if (Input.GetKey (KeyCode.A)) 
+		{
+			Turret.gameObject.transform.Rotate( new Vector3(0, 0, RotateSpeed * Time.deltaTime), Space.Self );
+		}
+	}
+	// ================================================================================================ //
+}
