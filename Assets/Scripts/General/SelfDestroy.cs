5200da558681303d8cc5c8d20dcd6362630d40dc
galhajaj <galhajaj@gmail.com>
2016-03-17 00:05:26 +0200

    add bullet flash & hit effects + add sounds that doesn't work

---
+++ Assets/Scripts/General/SelfDestroy.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfDestroy : MonoBehaviour 
+{
+	public float LifeTime;
+	// ================================================================================================ //
+	void Start () 
+	{
+		Destroy(gameObject, LifeTime);
+	}
+	// ================================================================================================ //
+}
--- Assets/Scripts/Tank/ParallelMagShooting.cs
+++ Assets/Scripts/Tank/ParallelMagShooting.cs
@@ -7,6 +7,9 @@ public class ParallelMagShooting : MonoBehaviour
 	public float ShootingInterval;
 	public Transform Nuzzle;
 	public float BulletForce;
+	public Transform Flash;
+	public GameObject HitEffect;
+	public float ShootDistance;
 
 	private float _lastTimeShooting;
 	// ================================================================================================ //
@@ -18,21 +21,39 @@ public class ParallelMagShooting : MonoBehaviour
 	void FixedUpdate () 
 	{
 		float timePassedfFromLastShot = Time.time - _lastTimeShooting;
-		if (Input.GetKey(FireKey) && timePassedfFromLastShot > ShootingInterval) 
+		if (Input.GetKey (FireKey) && timePassedfFromLastShot > ShootingInterval) 
 		{
+			// show flash effect
+			Flash.GetComponent<Renderer>().enabled = true;
+
 			_lastTimeShooting = Time.time;
 
 			Vector2 nuzzleRotation = Nuzzle.localRotation * Nuzzle.up;
 
-			RaycastHit2D hit = Physics2D.Raycast(Nuzzle.position, nuzzleRotation, 100, ~(1 << LayerMask.NameToLayer("TankLayer")));
+			RaycastHit2D hit = Physics2D.Raycast (Nuzzle.position, nuzzleRotation, ShootDistance, ~(1 << LayerMask.NameToLayer ("TankLayer")));
+
 			if (hit.collider != null) 
 			{
+				// hit effect
+				Instantiate (HitEffect, hit.point, Quaternion.identity);
+
 				Rigidbody2D collidedRigidBody = hit.collider.gameObject.GetComponent<Rigidbody2D> ();
 				if (collidedRigidBody != null) 
 				{
-					collidedRigidBody.AddForce(nuzzleRotation * BulletForce);
+					collidedRigidBody.AddForce (nuzzleRotation * BulletForce);
 				}
+			} 
+			else 
+			{
+				// hit effect
+				Vector3 hitPosition = Nuzzle.position + new Vector3(nuzzleRotation.x, nuzzleRotation.y, 0.0F) * ShootDistance;
+				Instantiate (HitEffect, hitPosition, Quaternion.identity);
 			}
+		} 
+		else 
+		{
+			// hide flash effect
+			Flash.GetComponent<Renderer>().enabled = false;
 		}
 	}
 	// ================================================================================================ //
