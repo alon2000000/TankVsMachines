0405183f2bda92db51bc5624347daf594856a534
galhajaj <galhajaj@gmail.com>
2016-03-20 23:55:25 +0200

    add missile launchers textures + change mouse cursor to target

--- Assets/Scripts/General/InputManager.cs
+++ Assets/Scripts/General/InputManager.cs
@@ -3,11 +3,11 @@ using System.Collections;
 
 public class InputManager : MonoBehaviour 
 {
-
+	public Texture2D CursorTexture;
 	// ================================================================================================ //
 	void Start () 
 	{
-	
+		Cursor.SetCursor (CursorTexture, new Vector2(16.0F, 16.0F), CursorMode.Auto);
 	}
 	// ================================================================================================ //
 	void Update () 
