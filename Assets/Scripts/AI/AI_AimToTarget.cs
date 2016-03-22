f05dbc09124cbce0218793f482dd848dfe8e97b8
galhajaj <galhajaj@gmail.com>
2016-03-22 23:09:32 +0200

    add random static enemies at radius + they aim in certain range

---
+++ Assets/Scripts/AI/AI_AimToTarget.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI_AimToTarget : MonoBehaviour 
+{
+	public Transform Turret;
+	public float DetectionRange;
+
+	private GameObject _playerTank = null;
+
+	// ================================================================================================ //
+	void Start () 
+	{
+
+	}
+	// ================================================================================================ //
+	void Update () 
+	{
+		if (_playerTank == null)
+			_playerTank = getPlayerTank ();
+		if (_playerTank == null)
+			return;
+
+		Vector3 playerPos = _playerTank.transform.position;
+
+		if (Vector3.Distance (playerPos, transform.position) > DetectionRange)
+			return;
+
+		float speed = -90.0F;
+		float step = speed * Time.deltaTime;
+
+		float angle = Mathf.Atan2 (transform.position.x - playerPos.x, transform.position.y - playerPos.y) * Mathf.Rad2Deg;
+
+		Quaternion rotateTo = Quaternion.AngleAxis(-angle, Vector3.forward);
+
+		Turret.rotation = Quaternion.RotateTowards(Turret.rotation, rotateTo, step);
+	}
+	// ================================================================================================ //
+	private GameObject getPlayerTank()
+	{
+		GameObject[] gos = GameObject.FindObjectsOfType(typeof(GameObject)) as GameObject[];
+		foreach (GameObject go in gos) 
+		{
+			if ( go.layer == LayerMask.NameToLayer("TankLayer") )
+				return go;
+		}
+		return null;
+	}
+	// ================================================================================================ //
+}
---
+++ Assets/Scripts/General/LevelCreator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCreator : MonoBehaviour 
+{
+	public GameObject Enemy;
+	public int EnemiesNumber;
+	public int EnemiesRadius;
+
+	// ================================================================================================ //
+	void Start () 
+	{
+		for (int i = 0; i < EnemiesNumber; i++) 
+		{
+			Vector3 pos = Random.insideUnitCircle * EnemiesRadius;
+			Instantiate (Enemy, pos, Quaternion.identity);
+		}
+	}
+	// ================================================================================================ //
+	void Update () 
+	{
+	
+	}
+	// ================================================================================================ //
+}
